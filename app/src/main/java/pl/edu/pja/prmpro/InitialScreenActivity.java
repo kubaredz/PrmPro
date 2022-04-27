bd6576e153466e58d1b50308671949080e5fbe7d
Jakub Redzinski <redzinski@protonmail.ch>
2022-04-27 17:34:21 +0200

    Adding activity initial screen

---
+++ app/src/main/java/pl/edu/pja/prmpro/InitialScreenActivity.java
@@ -0,0 +1,14 @@
+package pl.edu.pja.prmpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitialScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_screen);
+    }
+}
